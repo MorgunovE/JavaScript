7a00aa3a254d133380b5c2ffb38da49f70bc317a
morgunove <morgunove87@gmail.com>
2021-09-04 21:17:34 +0500

    lyndaGames

---
+++ lyndaGAMES/js/main.js
@@ -0,0 +1,51 @@
+let dragAndDrop = (function () {
+  let myX = '',
+    myY = '',
+    whichArt = ''
+  
+  function resetZ() {
+    let elements = document.querySelectorAll('img')
+    for (let i = elements.length - 1; i >= 0; i--) {
+      elements[i].style.zIndex = 5;
+    }
+  }
+  
+  function moveStart(e) {
+    whichArt = e.target;
+    myX = e.offsetX === undefined ? e.layerX : e.offsetX;
+    myY = e.offsetY === undefined ? e.layerY : e.offsetY;
+    resetZ();
+    whichArt.style.zIndex = 10;
+  }
+  
+  function moveDragOver(e) {
+    e.preventDefault();
+  }
+  
+  function moveDrop(e) {
+    e.preventDefault();
+    whichArt.style.left = e.pageX - myX + 'px';
+    whichArt.style.top = e.pageY - myY + 'px';
+  }
+  
+  function touchStart(e) {
+    e.preventDefault();
+    let whichArt = e.target;
+    let touch = e.touches[0];
+    let moveOffsetX = whichArt.offsetLeft - touch.pageX;
+    let moveOffsetY = whichArt.offsetTop - touch.pageY;
+    resetZ()
+    whichArt.style.zIndex = 10;
+    whichArt.addEventListener('touchmove', function () {
+      let positionX = touch.pageX + moveOffsetX;
+      let positionY = touch.pageY + moveOffsetY;
+      whichArt.style.left = positionX + 'px';
+      whichArt.style.top = positionY + 'px';
+    }, false)
+  }
+  
+  document.querySelector('body').addEventListener('dragstart', moveStart, false)
+  document.querySelector('body').addEventListener('dragover', moveDragOver, false)
+  document.querySelector('body').addEventListener('drop', moveDrop, false)
+  document.querySelector('body').addEventListener('touchstart', touchStart, false)
+})();
